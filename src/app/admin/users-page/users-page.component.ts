0a282250f60cdb8cf71716fdd72818e4cabbe6cf
GLitrico <59063578+Urdiale@users.noreply.github.com>
2023-11-13 16:38:21 -0300

    feat: api and list image

--- src/app/admin/users-page/users-page.component.ts
+++ src/app/admin/users-page/users-page.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./users-page.component.scss']
 })
 export class UsersPageComponent {
-
+  constructor(private http: HttpClient) { }
+  title = 'api-angular';
+  dataImages = [{url:'teste', title: ''}];
+  ngOnInit() {
+    this.http
+      .get<any>('https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=20')
+      .subscribe(data => {
+        this.dataImages = data.photos;
+        console.log(this.dataImages);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http'; // importing the http module
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
